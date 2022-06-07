c7321d598426d7113a92ae5231e91cb449f7503c
Nkumah7 <67166961+Nkumah7@users.noreply.github.com>
2022-06-07 08:29:16 +0100

    Update python_syntax_and_variables.py

---
+++ Python lecture 1/python_syntax_and_variables.py
@@ -0,0 +1,143 @@
+# 1. Print
+# print("My name is Jo and I am a master's student")
+
+# 2. Strings
+# print('There was traffic today')
+
+# 3. Variables
+# x = 'There was traffic today'
+# print(x)
+
+# Example
+# The top five football leagues
+
+
+# english = "English Premier league"
+# spain = "La Liga"
+# italy = "Serie A"
+# germany = "Bundesliga"
+# france = "Ligue 1"
+
+# print("The top five leagues are: ")
+# print("1. ")
+# print(english)
+# print("2.")
+# print(spain)
+# print("3. ")
+# print(italy)
+# print("4. ")
+# print(germany)
+# print("5. ")
+# print(france)
+
+# English = "EPL"
+# print(English)
+
+# 4. Numbers
+# integer (int) - 1 2 3 4 5 6 10 20 -1 -2 -10 -100
+# floating-point (float) - 0.1, 10.0, 4.5, 20.5, -100.6, -1000.5
+
+# print(4.5)
+height = 189.5
+weight = 89.0
+
+# print(height, weight, "male", sep=", ")
+
+# 5. Calculations
+# addition
+# print(189.5 + 89.0)
+
+# # subtraction
+# print(189.5 - 89.0)
+
+# # multiplication
+# print(189.5 * 89.0)
+
+# # Division
+# print(189.5 / 89.0)
+
+# # modulus
+# print(189.5 % 89.0)
+
+# addition
+add = height + weight
+
+# subtraction
+sub = height - weight
+
+# multiplication
+mul = height * weight
+
+# Division
+div = height / weight
+
+# modulus
+mod = height % weight
+
+# Exponent
+exp = height ** 10
+
+# print(add, sub, mul, div, mod, exp, sep=", ")
+
+# PEMDAS - Parenthesis, Exponent, Multiplication, Division, Addition
+# Subtraction
+# print(25 * 68 + 13 / 28)
+# print(20/0)
+
+# Changing Numbers
+# name = "Jo"
+# name = 10
+# name = 20
+# print(name)
+
+# 6. Concatenation
+# first_name = "Lionel"
+# last_name = "Messi"
+
+# full_name = first_name + " " + last_name
+# # print(full_name)
+
+# # Plus/minus/times/division Equals
+# lionel_age = 35
+
+# # lionel_age = lionel_age + 5
+# lionel_age += 5
+# lionel_desc = "Lionel messi will be" 
+# print(lionel_desc, lionel_age, "in 5 years time")
+# lionel_age -= 10
+# print(full_name, "was", lionel_age, "5 years ago")
+# lionel_age *= 2
+
+# # String Casting
+# lionel_in_30_years = full_name + " will be " + str(lionel_age) + " in 30 years"
+# # int/float casting
+# print(lionel_in_30_years)
+
+# name = "Cristiano"
+# print(name)
+# name += " Ronaldo"
+# print(name)
+# name += " is a great player"
+# print(name)
+
+
+# "djbcdwcdcd;',;'s,ksndjnkcndjnqkdmklsdnckjcNLNJDSBJDNSCJDSNCKNDCDK \
+# jbvjdvjds"
+# 7. Multi-line strings
+# x = """dwdhcdoiwoiwhjuwhfoiw/d;wjijdiwjfijenkdsncjdncjdnjcndjsncjw
+# dfnjdwnjdwbfjewnfjkwnwfknbrfj
+# acmbdhjsbfktml,flm
+# jdfhwbfkmf,f;'
+# jbewhfwel,f
+# """
+
+# print(x)
+
+# User input
+name = input("What is your name? ")
+print(f"Welcome {name} to JoTeq Dojo")
+
+age = input("How old are you? ")
+# print(type(age))
+a = float(age) + 2
+print(f"I am {age} years")
