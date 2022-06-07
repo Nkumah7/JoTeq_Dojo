04c60f4e8f206c6f78b5b8d02a2655dd3940964d
Nkumah7 <67166961+Nkumah7@users.noreply.github.com>
2022-06-07 08:54:19 +0100

    Update python_syntax_and_variables_proj.py

--- Python lecture 1/Project/python_syntax_and_variables_proj.py
+++ Python lecture 1/Project/python_syntax_and_variables_proj.py
@@ -7,8 +7,19 @@ you will investigate how different factors such as age, sex, BMI, etc. affect th
 
 """
 
+
 # 1. Create variables for each factor we will consider when estimating medical insurance costs
 # age, sex(0, 1), bmi (body mass index), num_of_children, smoker (0, 1)
+# create the initial variables below
+# age = 28
+# sex = 0
+# bmi = 26.2
+# num_of_children = 3
+# smoker = 0
+
+
+# Add insurance estimate formula below
+# insurance_cost = 250 * age - 128 * sex + 370 * bmi + 425 * num_of_children + 24000 * smoker - 12500
 
 
 # 2. Create variable to calculate insurance cost 
